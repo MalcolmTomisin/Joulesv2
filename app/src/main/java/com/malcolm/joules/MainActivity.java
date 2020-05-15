4f305f0a9aba47be031060b38815725fe16769d7
Malcolmtomisin <malcolmtomisin@gmail.com>
2020-05-15 15:52:21 +0100

    animation wip and drawerlayout

--- app/src/main/java/com/malcolm/joules/MainActivity.java
+++ app/src/main/java/com/malcolm/joules/MainActivity.java
@@ -1,14 +1,12 @@
 package com.malcolm.joules;
 
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/malcolm/joules/SplashActivity.java
+++ app/src/main/java/com/malcolm/joules/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.malcolm.joules;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -12,20 +13,23 @@ import com.malcolm.joules.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final int SPLASH_TIME = 3000;
+    private static final int SPLASH_TIME = 7000;
     private ActivitySplashBinding binding;
-
+    private AnimationDrawable logoStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySplashBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        binding.imageView6.setBackgroundResource(R.drawable.logo);
+        logoStart = (AnimationDrawable) binding.imageView6.getBackground();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        logoStart.start();
         new Handler().postDelayed(
                 () -> {
                     Intent decideIntent = new Intent(SplashActivity.this,MainActivity.class);
