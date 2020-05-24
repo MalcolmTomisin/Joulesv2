06e3771524cf6ec58ed6f225ab4fceeb32ca9400
Malcolmtomisin <malcolmtomisin@gmail.com>
2020-05-24 22:07:50 +0100

    album screen fragment

---
+++ app/src/main/java/com/malcolm/joules/fragments/AlbumDetailFragment.java
@@ -0,0 +1,7 @@
+package com.malcolm.joules.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class AlbumDetailFragment extends Fragment {
+    long albumID = -1;
+}
--- app/src/main/java/com/malcolm/joules/utiils/NavigationUtils.java
+++ app/src/main/java/com/malcolm/joules/utiils/NavigationUtils.java
@@ -1,6 +1,8 @@
 package com.malcolm.joules.utiils;
 
 import android.app.Activity;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
 import android.util.Pair;
 import android.view.View;
 
@@ -8,9 +10,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.malcolm.joules.R;
+
 public class NavigationUtils {
     public static void navigateToAlbum(Activity context, long albumID, Pair<View, String> transitionViews){
         FragmentTransaction transaction = ((AppCompatActivity) context).getSupportFragmentManager().beginTransaction();
         Fragment fragment;
+
+        Transition changeImage = TransitionInflater.from(context).inflateTransition(R.transition.image_transform);
+        transaction.addSharedElement(transitionViews.first,transitionViews.second);
     }
 }
